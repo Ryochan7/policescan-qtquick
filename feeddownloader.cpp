55b6d9cbf08e9e98c80309e79e6d9e8a9171c7e1
Travis Nickles <nickles.travis@gmail.com>
2019-06-19 21:23:03 -0500

    Changed feed downloader due to site change

--- feeddownloader.cpp
+++ feeddownloader.cpp
@@ -1,3 +1,5 @@
+#include <QUrlQuery>
+
 #include "feeddownloader.h"
 
 FeedDownloader::FeedDownloader(QObject *parent) : QObject(parent)
@@ -7,17 +9,30 @@ FeedDownloader::FeedDownloader(QObject *parent) : QObject(parent)
 
 void FeedDownloader::startScanFeed(int feedId)
 {
-    QNetworkRequest request(QUrl(QString("http://m.broadcastify.com/?feedId=%1").arg(feedId)));
-    QNetworkReply *reply = manager->get(request);
+    QNetworkRequest request(QUrl(QString("http://www.broadcastify.com/listen/webpl.php?feedId=%1").arg(feedId)));
+    QUrl params;
+    QUrlQuery query;
+    query.addQueryItem("t", "14");
+    params.setQuery(query);
+    QByteArray tempBa;
+    tempBa.append("t=14");
+    //tempBa = params.toEncoded(QUrl::RemoveFragment);
+    request.setHeader(QNetworkRequest::ContentTypeHeader,
+        "application/x-www-form-urlencoded");
+    request.setRawHeader("webAuth", "74f440ad812f0cc2192ab782e27608cc");
+    QNetworkReply *reply = manager->post(request, tempBa);
+    //QNetworkReply *reply = manager->get(request);
     connect(reply, &QNetworkReply::finished, this, &FeedDownloader::processFeedReply);
 }
 
 void FeedDownloader::processFeedReply()
 {
-    QNetworkReply *reply = (QNetworkReply*)QObject::sender();
+    QNetworkReply *reply = static_cast<QNetworkReply*>(QObject::sender());
     if (reply->error() == QNetworkReply::NoError)
     {
-        QString url = parseFeedUrl(QString(reply->readAll()));
+        QString pageText = QString(reply->readAll());
+        //qDebug() << pageText;
+        QString url = parseFeedUrl(pageText);
         if (!url.isEmpty())
         {
             emit feedReady(url);
